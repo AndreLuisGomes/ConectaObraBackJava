c7eb3457cf63c0d930a59c5b69bb92ab816b09b5
AndreLuisGomes <andreluisgomes09@gmail.com>
2026-02-19 14:38:15 -0400

    refactor(controller): refatorando método de login no AuthController

--- src/main/java/com/conectaobra/conectaobra/controllers/AuthController.java
+++ src/main/java/com/conectaobra/conectaobra/controllers/AuthController.java
@@ -7,7 +7,10 @@ import com.conectaobra.conectaobra.models.Role;
 import com.conectaobra.conectaobra.models.Usuario;
 import com.conectaobra.conectaobra.services.RoleService;
 import com.conectaobra.conectaobra.services.UsuarioService;
+import com.nimbusds.jwt.JWTClaimsSet;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -38,26 +41,36 @@ public class AuthController {
     private final UsuarioService usuarioService;
     private final RoleService roleService;
 
+    // JWT Constantes \\
+
+    private final long AUTH_REFRESH_TOKEN = 86400L;
+    private final long AUTH_TOKEN = 300L;
+
     // Método de login \\
 
     @PostMapping("logar")
     ResponseEntity<Object> logar(@RequestBody UsuarioLoginDTO usuarioLoginDTO){
 
-        // Verifica e valida se o login e a senha estão preenchidas e não são vazias \\
-        if(usuarioLoginDTO.nome().isBlank() || usuarioLoginDTO.senha().isBlank()){
-            return ResponseEntity.badRequest().body("Envie uma requisição válida!");
+        // Verificar se os campos não são nulos \\
+        if(!usuarioService.camposLoginCorretos(usuarioLoginDTO)){
+            return ResponseEntity.status(400).body("Campo usuário ou senha não podem ser vazios!");
         }
-        Optional<Usuario> usuarioDoBanco = this.usuarioService.obterPorNome(usuarioLoginDTO.nome());
+        String nomeLogin = usuarioLoginDTO.nome().trim();
 
-        if(usuarioDoBanco.isEmpty() || !this.usuarioService.loginEstaCorreto(usuarioLoginDTO, passwordEncoder)){
-            return ResponseEntity.status(401).body("Usuário ou senha inválidos!");
-        }else{
+        // Verificar se os dados batem \\
+        Optional<Usuario> usuarioDoBanco = this.usuarioService.obterPorNome(nomeLogin);
 
-            return ResponseEntity.ok().body(
-                    new AuthResponse(usuarioLoginDTO.nome(),
-                            gerarJWT(usuarioDoBanco.get()).getTokenValue(),
-                            usuarioDoBanco.get().getRole().getNome()));
+        if(usuarioDoBanco.isPresent() &&
+                (usuarioDoBanco.get().getNome().equals(nomeLogin)) &&
+                (this.usuarioService.loginEstaCorreto(usuarioLoginDTO, passwordEncoder))
+        ){
+            return ResponseEntity.ok().body(new AuthResponse(nomeLogin,
+                    this.gerarJWT(usuarioDoBanco.get(), AUTH_TOKEN).getTokenValue(),
+                    this.gerarJWT(usuarioDoBanco.get(), AUTH_REFRESH_TOKEN).getTokenValue(),
+                    usuarioDoBanco.get().getRole().getNome()
+                    ));
         }
+        return ResponseEntity.status(401).body("Usuário ou senha inválidos!");
     }
 
     // Método de registrar \\
@@ -66,35 +79,41 @@ public class AuthController {
     ResponseEntity<Object> registrar(@RequestBody UsuarioDTO usuarioDTO){
         Optional<Usuario> usuario = this.usuarioService.obterPorNome(usuarioDTO.nome());
         Optional<Role> role = this.roleService.obterPorId(usuarioDTO.role().getId());
+
         if(role.isEmpty()){
             return ResponseEntity.status(400).body("Role inválida ou não existe!");
         }
         if(usuario.isEmpty()){
             Usuario novoUsuario = usuarioDTO.mapearParaUsuario();
             novoUsuario.setRole(role.get());
+            System.out.println(role);
             novoUsuario.setSenha(this.passwordEncoder.encode(novoUsuario.getSenha()));
             this.usuarioService.salvarUsuario(novoUsuario);
-            Usuario retornoUsuario = this.usuarioService.obterPorNome(novoUsuario.getNome()).get();
-            return ResponseEntity.ok().body(new AuthResponse(retornoUsuario.getNome(), this.gerarJWT(retornoUsuario).getTokenValue(), retornoUsuario.getRole().getNome()));
+            return ResponseEntity.ok().body(
+                    new AuthResponse(novoUsuario.getNome(),
+                            this.gerarJWT(novoUsuario, this.AUTH_TOKEN).getTokenValue(),
+                            this.gerarJWT(novoUsuario, this.AUTH_REFRESH_TOKEN).getTokenValue(),
+                            novoUsuario.getRole().getNome()));
         }
         return ResponseEntity.status(409).body("Nome já está em uso!");
     }
 
     // Método para gerar JWT \\
 
-    private Jwt gerarJWT(Usuario usuario){
-        var now = Instant.now();
-        var exp = 300L;
+    private Jwt gerarJWT(Usuario usuario, long exp){
+        Instant now = Instant.now();
+
+        String scope = usuario.getRole().getNome();
 
         Optional<Role> role = this.roleService.obterPorId(usuario.getRole().getId());
 
         // Claims: são os "componentes" que o JwtEncoder aceita para sua construção \\
-        var claims = JwtClaimsSet.builder()
+        JwtClaimsSet claims = JwtClaimsSet.builder()
                 .issuer(this.apiName)
                 .subject(usuario.getId().toString())
                 .issuedAt(now)
                 .expiresAt(now.plusSeconds(exp))
-//                .claims.put("role", List.of("ROLE_" + usuario.getRole().getNome()))
+                .claim("scope", scope)
                 .build();
 
         return jwtEncoder.encode(JwtEncoderParameters.from(claims));
