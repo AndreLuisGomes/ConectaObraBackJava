cd7278f0217fd2197c2b7966337f18fa6dbbeaa8
AndreLuisGomes <andreluisgomes09@gmail.com>
2026-02-20 17:27:32 -0400

    refactor(dtos): alterando lógica de validação para o record de usuarioLoginDTO

--- src/main/java/com/conectaobra/dtos/UsuarioLoginDTO.java
+++ src/main/java/com/conectaobra/dtos/UsuarioLoginDTO.java
@@ -1,5 +1,9 @@
 package com.conectaobra.dtos;
 
-public record UsuarioLoginDTO(String nome, String senha) {
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
 
+public record UsuarioLoginDTO(
+        @Length(min = 3, message = "Nome deve ter mais de 3 caracteres") @NotBlank(message = "Nome deu errado") String nome,
+        @Length(min = 3, message = "Senha deve ter mais de 3 caracteres") String senha) {
 }
